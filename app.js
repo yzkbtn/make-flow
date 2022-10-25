d7e515918f267e9b72c0635cad83e97cf0773b15
DESKTOP-98V9SEK\acer <yzkbtn@gmal.com>
2022-10-24 18:57:00 -0700

    完了未完了の実装が完了しました。

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ $("#addTodo").click(function() {
 
   $("input").val("");
 });
+
+$(document).on("change", "input[type=checkbox]", function(){
+  if($(this).is(":checked")) {
+    $(this).parent().css("text-decoration","line-through");
+  } else {
+    $(this).parent().css("text-decoration","none");
+  }
+  
+});
